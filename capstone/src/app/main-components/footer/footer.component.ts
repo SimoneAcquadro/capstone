4c9901572b1c132280ac0878da7fcf45a2d1f765
SimoneAcquadro <siyhfe@icloud.com>
2024-06-27 12:43:37 +0200

    navbar e footer finzionanti

--- capstone/src/app/main-components/footer/footer.component.ts
+++ capstone/src/app/main-components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  currentYear: number;
+
+  constructor() {
+    const today = new Date();
+    this.currentYear = today.getFullYear();
+  }
+
+  isShow: boolean = false;
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const viewportHeight = window.innerHeight;
+    const scrollHeight = document.documentElement.scrollHeight;
+
+    this.isShow = scrollPosition > (scrollHeight - viewportHeight) * 0.3;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
 }
